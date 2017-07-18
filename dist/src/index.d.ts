8377c17ef18341419d6d8a2b87c92ccfb5f74328
Justin Lettau <justin.b.lettau@gmail.com>
2017-07-18 14:11:46 -0400

    chore: add root barrel

---
+++ dist/src/index.d.ts
@@ -0,0 +1,36 @@
+/**
+ * Get object property value.
+ *
+ * @param obj Object to get value from.
+ * @param path Dot notation string.
+ * @param value Optional default value to return if path is not found.
+ */
+export declare function get(obj: Object, path: string, value?: any): any;
+/**
+ * Set object property value.
+ *
+ * @param obj Object to set value for.
+ * @param path Dot notation string.
+ * @param value Value to set at path.
+ */
+export declare function set(obj: Object, path: string, value: any): void;
+/**
+ * Check if object has property value.
+ *
+ * @param obj Object to set value for.
+ * @param path Dot notation string.
+ */
+export declare function has(obj: Object, path: string): boolean;
+/**
+ * Delete a property from an object.
+ *
+ * @param obj Object to set value for.
+ * @param path Dot notation string.
+ */
+export declare function remove(obj: Object, path: string): boolean;
+/**
+ * Get all dot notations paths from an object.
+ *
+ * @param obj Object to get paths for.
+ */
+export declare function paths(obj: Object): string[];
