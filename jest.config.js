b485830a8998795a45a464a86e444bd8f6bb3de6
Justin Lettau <justin.b.lettau@gmail.com>
2021-08-31 09:36:27 -0700

    revert "feat: output esm"
    This reverts commit cc954c81022b6945739052e041ac544cfb72181e.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
   roots: ['<rootDir>/src/'],
-  transformIgnorePatterns: ['/node_modules/(?!ts-util-is)'],
 };
